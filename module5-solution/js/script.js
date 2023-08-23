f2aa4c7c43549521aca7f6d134ed124b6e7839bb
SenpaiMarques <140733034+SenpaiMarques@users.noreply.github.com>
2023-08-23 17:01:10 +0100

    changes

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -154,7 +154,7 @@ dc.loadMenuCategories = function () {
 dc.loadMenuItems = function (categoryShort) {
   showLoading("#main-content");
   $ajaxUtils.sendGetRequest(
-    menuItemsUrl + categoryShort + ".json",
+    menuItemsUrl + categoryShort,
     buildAndShowMenuItemsHTML);
 };
 
